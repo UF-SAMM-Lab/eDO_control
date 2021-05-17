352cedd37fe1b10a452615f447c113deccb5c68f
jaredtflowers <jared.t.flowers@gmail.com>
2021-05-17 13:03:51 -0400

    added file for use with the GUI

---
+++ calibrate.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+master_ip="10.42.0.49"
+hostname=`hostname`
+
+vars="export ROS_MASTER_URI=\"http://${master_ip}:11311\";"
+vars=$vars" export ROS_HOSTNAME=\"${hostname}.local\";"
+vars=$vars" export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\";"
+vars=$vars" cd \"catkin_ws_data_collection_1\";"
+vars=$vars" source \"devel/setup.bash\";"
+vars=$vars" roslaunch edo_control calibrate.launch;"
+
+bash --rcfile <(cat ~/.bashrc; echo "${vars}")
+
